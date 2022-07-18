7cf0793fb21a0b715bfc630aa31e032181c8487c
Franco Olmi <dev9.sitenso@tensolite.com>
2022-07-18 19:22:00 -0300

    se maqueto las primeras vistas del home

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,8 +1,111 @@
-import React from 'react'
+import { Button } from "@mui/material";
+import React from "react";
+
+// IMAGENES
+import portada from "../assets/images/maqueta/portada.jpg";
+import producto from "../assets/images/maqueta/envases10.jpg";
+
+// IMPORTACIONES}
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 const home = () => {
   return (
-    <div><h1>home</h1></div>
-  )
-}
-export default home
+    <>
+      <div className="bg-img">
+        <h2>Bienvenido</h2>
+        <h1>
+          Jc<span>Cosmetic</span>
+        </h1>
+        <img src={portada} alt="Imagen de portada" />
+      </div>
+
+      <div className="container">
+        <div className="seccion-tarjetas">
+          <div className="tarjetas">
+            <div className="box">
+              <div className="imgBx">
+                <img src={producto} alt="Imgen de producto" />
+              </div>
+              <div className="content">
+                <div>
+                  <h2>Envases Pet</h2>
+                  <a href="/Productos">
+                    {" "}
+                    <Button>Ver mas</Button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="tarjetas">
+            <div className="box">
+              <div className="imgBx">
+                <img src={producto} alt="Imgen de producto" />
+              </div>
+              <div className="content">
+                <div>
+                  <h2>Gatillos Profesional</h2>
+                  <a href="/Productos">
+                    {" "}
+                    <Button>Ver mas</Button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="tarjetas">
+            <div className="box">
+              <div className="imgBx">
+                <img src={producto} alt="Imgen de producto" />
+              </div>
+              <div className="content">
+                <div>
+                  <h2>Convos</h2>
+                  <a href="/Productos">
+                    {" "}
+                    <Button>Ver mas</Button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+      {/* Seccion metodos */}
+      <div className="seccion-metodos">
+          <div>           
+            <p>
+            <AccountBalanceWalletIcon/>
+              <b>Opciones de pago</b>
+            </p>
+            <p>Visa, Mastercar, Transferencia, Mercado Pago</p>
+          </div>
+          <div>
+            <p>
+              <ThumbUpIcon/>
+              <b>100% Confiables</b>
+            </p>
+            <p>
+              Nos aseguramos que todos <br/>nuestros productos sean de la mejor
+              calidad
+            </p>
+          </div>
+          <div>
+            <p>
+              <LocalShippingIcon/>
+              <b>Metodos de Envios</b>
+            </p>
+            <p>
+              Nos aseguramos de enviar nuestros productos con <br/>
+              transportes de confiaza y mas conviniente al cliente
+            </p>
+          </div>
+        </div>
+    </>
+  );
+};
+export default home;
